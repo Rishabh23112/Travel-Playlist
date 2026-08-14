ef35cd797556e02dd90da8221243d773a7261658
Rishabh23112 <irishabh.k2311@gmail.com>
2026-08-14 11:56:08 +0530

    changed background

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,8 @@ import AudioPlayer from './AudioPlayer'
 
 const Hero = () => {
   return (
-    <div  className='h-screen w-full bg-[url(/images/travel.png)]'>
-        {/* <video src="/video/travelvid-compressed.mp4" autoPlay muted loop playsInline className="absolute top-0 left-0 w-full h-full object-cover z-0"> <source src=''/></video> */}
+    <div  className='h-screen w-full '>
+        <video src="/video/285224.mp4" autoPlay muted loop playsInline disablePictureInPicture className="absolute top-0 left-0 w-full h-full object-cover z-0"> </video>
         <div className='text-white flex justify-center items-end h-screen py-8'>
             <AudioPlayer/>
         </div>
