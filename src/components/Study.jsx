95c6ee42e06af892d0b49dd6cf23e9bd90fde386
Rishabh23112 <irishabh.k2311@gmail.com>
2026-08-15 21:46:05 +0530

    changed background

--- src/components/Study.jsx
+++ src/components/Study.jsx
@@ -5,7 +5,7 @@ const Study = ({ isActive = false }) => {
   return (
     <div>
       <div className='h-screen w-full '>
-        <video src="https://cdn.pixabay.com/video/2026/03/27/342901_large.mp4" autoPlay muted loop playsInline disablePictureInPicture className="absolute top-0 left-0 w-full h-full object-cover z-0"> </video>
+        <video src="https://cdn.pixabay.com/video/2024/06/05/215407_large.mp4" autoPlay muted loop playsInline disablePictureInPicture className="absolute top-0 left-0 w-full h-full object-cover z-0"> </video>
         <div className='text-white flex justify-center items-end h-screen py-8'>
           <AudioPlayer songs={songs1Data.songs} isActive={isActive} />
         </div>
